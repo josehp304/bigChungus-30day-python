b03ebbc40d9511d85dc54c01e7a108dde5251008
Anand.a.jain <anandajain10@gmail.com>
2024-11-21 22:19:24 +0530

    Add files via upload

---
+++ islandboi/day_2_python.py
@@ -0,0 +1,13 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+odd = []
+even = []
+for num in numbers:
+    if num % 2 == 0:
+        even.append(num)  
+    else:
+        odd.append(num)  
+print("odd:", odd)
+print("even:", even)
+
+
+     
