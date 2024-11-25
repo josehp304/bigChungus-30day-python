89eefdc34405bce42b28b88b9b1eaf739ccda819
iam-dionsuby <iam.dionsuby@gmail.com>
2024-11-26 01:21:25 +0530

    Add files via upload

---
+++ dion/day_3.py
@@ -0,0 +1,13 @@
+n=int(input("Enter the total number of digits"))
+num=[]
+print("Enter ",n," numbers")
+for i in range (n):
+     number=int(input())
+     num.append(number)
+print("Numbers =",num)
+num.sort()
+print("Numbers in ascending order=",num)     
+if len(num)%2!=0:
+      print("Middle term=",num[n//2])
+else:
+      print("Middle terms are:",num[(n-1)//2],"and",num[((n-1)//2)+1])
