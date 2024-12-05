9311924d42bd0b80ba11840505980925dcbf0991
HANSTS13 <hanstomsojan2028@cs.sjcetpalai.ac.in>
2024-12-05 12:16:34 +0530

    Delete hans/day_13_python.py

--- hans/day_13_python.py
+++
@@ -1,7 +0,0 @@
-def area_perimeter(length,width):
-      print("Area= ",length*width)
-      print("Perimeter= ",2*(length+width))
-print("Enter length and breadth")      
-l=float(input())
-w=float(input())      
-area_perimeter(l,w)             
