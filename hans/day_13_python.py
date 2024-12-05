e074b700468881922a8fc82b3502a5ac61540a37
HANSTS13 <hanstomsojan2028@cs.sjcetpalai.ac.in>
2024-12-05 12:18:57 +0530

    Add files via upload

---
+++ hans/day_13_python.py
@@ -0,0 +1,7 @@
+def area_perimeter(length,width):
+      print("Area= ",length*width)
+      print("Perimeter= ",2*(length+width))
+print("Enter length and breadth")      
+l=float(input())
+w=float(input())      
+area_perimeter(l,w)             
